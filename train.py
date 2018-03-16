e30c8778025786d46dcc027e27f84a9fe53a54a1
sumakar <sumakarkvs@gmail.com>
2018-03-16 22:36:50 +0530

    Made more Generic

--- train.py
+++ train.py
@@ -14,12 +14,12 @@ data_cols=data.shape[1];
 #print(data[1:3,:])
 
 #
-train_data=data[0:int(data_rows*0.7),0:3];
-test_data=data[int(data_rows*0.7):(data_rows-1),0:3];
+train_data=data[0:int(data_rows*0.7),0:(data_cols-2)];
+test_data=data[int(data_rows*0.7):(data_rows-1),0:(data_cols-2)];
 
 #output
-train_data_o=data[0:int(data_rows*0.7),4];
-test_data_o=data[int(data_rows*0.7):(data_rows-1),4];
+train_data_o=data[0:int(data_rows*0.7),(data_cols-1)];
+test_data_o=data[int(data_rows*0.7):(data_rows-1),(data_cols-1)];
 
 clf = svm.SVC()
 clf.fit(train_data,train_data_o)
